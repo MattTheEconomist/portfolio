2a125cf92eb48827fc25f5b5d02d32dac2df2c45
Matt Ashford <mattashford1@gmail.com>
2021-10-22 10:35:57 -0400

    background working better

--- portfolio/src/ContentPages/LandingPage.jsx
+++ portfolio/src/ContentPages/LandingPage.jsx
@@ -6,9 +6,21 @@ export const LandingPage = () => {
   const [mousePoz, setMousePoz] = useState({ x: 0, y: 0 });
   const [time, setTime] = useState(Date.now());
 
-  const hunnit = d3.range(50);
+  const hunnit = d3.range(15);
 
-  let springPlural = useSprings(
+  const svgWidth = 2000;
+  const svgHeight = 900;
+
+  const timeRanges = [9000, 14000, 170000, 19000];
+
+  function selectRandomClassName() {
+    const circleColors = ["primary", "secondary", "third"];
+    const randomIndex = Math.floor(Math.random() * 3);
+
+    return `circle circle_${circleColors[randomIndex]}`;
+  }
+
+  const springPluralGroupOne = useSprings(
     hunnit.length,
     hunnit.map((item) => ({
       from: { x: 0, y: 0 },
@@ -16,35 +28,95 @@ export const LandingPage = () => {
         x: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
         y: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
       },
-      config: { duration: 20000 },
+      config: { duration: timeRanges[0] },
       loop: { reverse: true },
     }))
   );
 
-  const circles = springPlural.map((el, i) => (
-    <animated.circle
-      r="30"
-      cx={Math.random() * 50 * i}
-      cy={Math.random() * 50 * i}
-      fill="grey"
-      id={`circle_${i}`}
-      key={`circle_${i}`}
-      className="circle"
-      style={el}
-    />
-  ));
+  const springPluralGroupTwo = useSprings(
+    hunnit.length,
+    hunnit.map((item) => ({
+      from: { x: 0, y: 0 },
+      to: {
+        x: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
+        y: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
+      },
+      config: { duration: timeRanges[1] },
+      loop: { reverse: true },
+    }))
+  );
+
+  const springPluralGroupThree = useSprings(
+    hunnit.length,
+    hunnit.map((item) => ({
+      from: { x: 0, y: 0 },
+      to: {
+        x: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
+        y: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
+      },
+      config: { duration: timeRanges[2] },
+      loop: { reverse: true },
+    }))
+  );
+
+  const springPluralGroupFour = useSprings(
+    hunnit.length,
+    hunnit.map((item) => ({
+      from: { x: 0, y: 0 },
+      to: {
+        x: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
+        y: Math.random() * 10 * item * 10 * (Math.random() - 0.5),
+      },
+      config: { duration: timeRanges[3] },
+      loop: { reverse: true },
+    }))
+  );
+
+  console.log(
+    selectRandomClassName(),
+    selectRandomClassName(),
+    selectRandomClassName()
+  );
+  function mapOverSprings(inputSpring) {
+    return inputSpring.map((el, i) => (
+      <animated.circle
+        r="40"
+        cx={Math.random() * svgWidth}
+        cy={Math.random() * svgHeight}
+        fill="grey"
+        key={`circ${Math.random() * 1000}`}
+        className={selectRandomClassName()}
+        style={el}
+      />
+    ));
+  }
+
+  let circles = mapOverSprings(springPluralGroupOne);
+  circles = circles.concat(mapOverSprings(springPluralGroupTwo));
+  circles = circles.concat(mapOverSprings(springPluralGroupThree));
+  circles = circles.concat(mapOverSprings(springPluralGroupFour));
 
   return (
-    <div id="titleTextContainer">
-      <h1 id="landingTextName">Matt is my Name</h1>
-      <h1 id="landingTextOuter">
-        {" "}
-        <span id="landingTextData">Data Viz</span> is my game
-      </h1>
-
-      <svg id="landingBackground" width="1200px" height="1200px">
-        {circles}
-      </svg>
+    <div id="landingPageContainer">
+      <div id="svgContainer">
+        <svg
+          id="landingBackground"
+          // width={`${svgWidth}px`}
+          // height={`${svgHeight}px`}
+          width="100%"
+          height="100%"
+          fill="green"
+        >
+          {circles}
+        </svg>
+      </div>
+      <div id="titleTextContainer">
+        <h1 id="landingTextName">Matt is my Name</h1>
+        <h1 id="landingTextOuter">
+          {" "}
+          <span id="landingTextData">Data Viz</span> is my game
+        </h1>
+      </div>
     </div>
   );
 };
